19942f74fe82b00d35e7208ab70d6a96a7335a57
mercklyzer <mercklyzer@gmail.com>
2022-11-23 19:48:19 +0800

    Add flow test

--- cypress/e2e/pokemon.page.cy.ts
+++ cypress/e2e/pokemon.page.cy.ts
@@ -21,35 +21,15 @@ describe('Pokemon Page', () => {
         })
     })
 
-
     it("Correct route", () => {
         cy.get('@path')
             .should('match', /^\/pokemons\/[1-9]{1}\d?\d?/g)
     })
 
     it("Pokemon Basic Info Exists", () => {
-        pokemonPage.elements.basicInfo()
-            .should($info => {
-                expect($info).to.contain('ID')
-                expect($info).to.contain('Height')
-                expect($info).to.contain('Weight')
-                expect($info).to.contain('Abilities')
-                expect($info).to.contain('Type')
-            })
+        pokemonPage.verifyBasicInfo()
         
-            // ^Another syntax:
-                // .should('contain', 'ID')
-                // .should('contain', 'Height')
-                // .should('contain', 'Weight')
-                // .should('contain', 'Abilities')
-                // .should('contain', 'Type')
 
-            // ^Another syntax:
-                // .should('contain', 'ID')
-                // .and('contain', 'Height')
-                // .and('contain', 'Weight')
-                // .and('contain', 'Abilities')
-                // .and('contain', 'Type')
 
         pokemonPage.elements.id().invoke('text')
             .then(text => {
@@ -81,21 +61,6 @@ describe('Pokemon Page', () => {
     })
 
     it('Pokemon Stats Table Exists', () => {
-        pokemonPage.elements.hpHeader().should('exist')
-        pokemonPage.statsShouldBeNumbers();
-
-        pokemonPage.elements.minBtn().click()
-        cy.wait(3000)
-        pokemonPage.statsShouldBeNumbers();
-
-        pokemonPage.elements.maxBtn().click()
-        cy.wait(3000)
-        pokemonPage.statsShouldBeNumbers();
-
-        pokemonPage.elements.baseBtn().click()
-        cy.wait(3000)
-        pokemonPage.statsShouldBeNumbers();
-
-
+        pokemonPage.verifyStats()
     })
 })
